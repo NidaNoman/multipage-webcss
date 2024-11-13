65a132450aa3ffcac042ce5a559d731566adb547
NidaNoman <mrsajjad3455@gmail.com>
2024-11-13 13:19:33 +0500

    vercel error solving

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 const About = () => {
   return (
     <div className="min-h-screen">
-
       <main className="main">
         <div className="about-container">
           <h1 className="title">About Skin Bliss</h1>
@@ -14,7 +13,7 @@ const About = () => {
           </p>
           <p className="paragraph">
             Our team of skincare enthusiasts curates the best products and shares expert tips to
-            guide you on your skincare journey. Whether you're looking for solutions for acne,
+            guide you on your skincare journey. Whether you&apos;re looking for solutions for acne,
             hydration, anti-aging, or simply want to pamper yourself, we have something for you.
           </p>
           <p className="paragraph">
@@ -24,15 +23,14 @@ const About = () => {
           </p>
           <p className="paragraph">
             Join our community of skincare lovers and embark on your journey to glowing skin.
-            Remember, every skin type is unique, and we are here to support you every step of the
-            way!
+            Remember, every skin type is unique, and we&apos;re here to support you every step of the
+            way.
           </p>
           <p className="paragraph">
-            Thank you for choosing Skin Bliss, where your skincare journey begins!
+            Thank you for choosing Skin Bliss, where your skincare journey begins.
           </p>
         </div>
       </main>
-
     </div>
   );
 };
